a030bd8893c78cfb33d24a8045d4390f1ed925bd
KeniierM <keniier.321@gmail.com>
2022-09-06 11:26:15 -0500

    inicio

---
+++ JuegoWeb/app/Http/Controllers/SalaJugadoresController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SalaJugadores;
+use Illuminate\Http\Request;
+
+class SalaJugadoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SalaJugadores  $salaJugadores
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SalaJugadores $salaJugadores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SalaJugadores  $salaJugadores
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SalaJugadores $salaJugadores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SalaJugadores  $salaJugadores
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SalaJugadores $salaJugadores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SalaJugadores  $salaJugadores
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SalaJugadores $salaJugadores)
+    {
+        //
+    }
+}
